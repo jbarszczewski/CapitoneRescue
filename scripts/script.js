06b7c1fe7ae30b2d1d0c1538bdec5a3e293ac824
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2020-11-29 16:48:40 +0000

    prototype

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const Scene = require('Scene');
+const Patches = require('Patches');
+
+(async function() {
+const [scoreText, score] = await Promise.all([
+  Scene.root.findFirst('scoreText'),
+  Patches.outputs.getScalar('score')
+]);
+
+scoreText.text = score.toString();
+
+})();
