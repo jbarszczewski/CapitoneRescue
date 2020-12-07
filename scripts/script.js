3fb5484b895710632eb95c3de66390e4e48a2cb9
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2020-12-07 22:20:19 +0000

    wired new assets and random coin positions

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
+const Diagnostics = require('Diagnostics');
 const Scene = require('Scene');
 const Patches = require('Patches');
 
 (async function() {
 const [scoreText, score] = await Promise.all([
   Scene.root.findFirst('scoreText'),
-  Patches.outputs.getScalar('score')
+  Patches.outputs.getScalar('score'),
 ]);
-
 scoreText.text = score.toString();
-
+Diagnostics.log('start');
+const speed = score.pinLastValue();
+Diagnostics.log(`Last val: ${speed}Speed is: ${speed / 2}`);
+await Patches.inputs.setScalar('netSpeed', speed / 2);
+Diagnostics.log('end');
 })();
